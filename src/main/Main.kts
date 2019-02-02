0529a59800035c53d13812911d2d7aab2ecfdff2
nahida <nc1517@ic.ac.uk>
2019-02-02 19:02:30 +0000

    pom file modified to include antlr and gen source directories

--- src/main/Main.kts
+++ src/main/Main.kts
@@ -1,12 +1,11 @@
-package wacc_25
-import org.antlr.v4.runtime.ANTLRInputStream
+//import org.antlr.v4.runtime.ANTLRInputStream
 import org.antlr.v4.runtime.CommonTokenStream
 
 fun main() {
-    val input = ANTLRInputStream(System.in)
+    val input = null
     val lexer = BasicLexer(input)
     val tokens = CommonTokenStream(lexer)
-    val parser = TParser(tokens)
-    val parseTree = parser.init()
+    val parser = BasicParser(tokens)
+    val parseTree = parser.prog()
     print(parseTree.toStringTree(parser))
 }
