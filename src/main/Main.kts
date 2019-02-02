cdb8925646b08b17d9442747cc1eb1d236c42a01
Blanca <bt3917@ic.ac.uk>
2019-02-02 19:04:13 +0000

    unresolved reference: antlr needs to be fixed

--- src/main/Main.kts
+++ src/main/Main.kts
@@ -3,10 +3,19 @@ import org.antlr.v4.runtime.ANTLRInputStream
 import org.antlr.v4.runtime.CommonTokenStream
 
 fun main() {
-    val input = ANTLRInputStream(System.in)
+    //char stream that reads from standard input
+    val input = ANTLRInputStream(java.lang.System.`in`)
+
+    //creates a lexer that feeds off of input charstream
     val lexer = BasicLexer(input)
+
+    //creates a buffer of tokens pulled from the lexer
     val tokens = CommonTokenStream(lexer)
-    val parser = TParser(tokens)
-    val parseTree = parser.init()
-    print(parseTree.toStringTree(parser))
+
+    //Creates a parser that feeds off of the tokens buffer
+    val parser = BasicParser(tokens)
+
+    val tree = parser.init()
+
+    System.out.println(tree.toString(parser))
 }
