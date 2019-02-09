6ccf9425b39bafc278b90b672e87b57b7243d957
nahida <nc1517@ic.ac.uk>
2019-02-09 18:08:24 +0000

    implemented println semanticcheck

--- src/main/kotlin/Nodes/Statement/PrintLnStatNode.kt
+++ src/main/kotlin/Nodes/Statement/PrintLnStatNode.kt
@@ -1,10 +1,14 @@
 package main.kotlin.Nodes.Statement
 
 import main.kotlin.ErrorLogger
+import main.kotlin.Errors.IncompatibleTypes
+import main.kotlin.Nodes.CharLitNode
 import main.kotlin.Nodes.Node
+import main.kotlin.Nodes.StringLitNode
 import main.kotlin.SymbolTable
 import src.main.kotlin.Nodes.ExprNode
 import kotlin.reflect.KClass
+import kotlin.system.exitProcess
 
 class PrintLnStatNode(val expr : ExprNode) : Node {
     override fun getType() : KClass<PrintLnStatNode> {
@@ -12,10 +16,12 @@ class PrintLnStatNode(val expr : ExprNode) : Node {
     }
 
     override fun syntaxCheck() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
     }
 
     override fun semanticCheck(errors: ErrorLogger, table: SymbolTable) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        if (expr !is StringLitNode && expr !is CharLitNode) {
+            errors.addError(IncompatibleTypes())
+        }
     }
 }
--- src/main/kotlin/WaccMain.kt
+++ src/main/kotlin/WaccMain.kt
@@ -8,7 +8,7 @@ import kotlin.system.exitProcess
 
 fun main(args: Array<String>) {
         if (args.size == 0) {
-                System.setIn(FileInputStream("../wacc_examples/invalid/semanticErr/exit/badCharExit.wacc"))
+                System.setIn(FileInputStream("../wacc_examples/invalid/semanticErr/print/printTypeErr01.wacc"))
         } else {
                 System.setIn(FileInputStream(args[0]))
         }
