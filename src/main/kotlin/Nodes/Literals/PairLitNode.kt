60936f4d8355a3d5c9ea8dc1757b2c4c7407cf5e
nahida <nc1517@ic.ac.uk>
2019-02-10 22:56:46 +0000

    added semantic check to free statement node

--- src/main/kotlin/Nodes/Literals/PairLitNode.kt
+++ src/main/kotlin/Nodes/Literals/PairLitNode.kt
@@ -4,9 +4,11 @@ import main.kotlin.ErrorLogger
 import main.kotlin.Nodes.BaseNode
 import main.kotlin.Nodes.Node
 import main.kotlin.SymbolTable
+import main.kotlin.Utils.LitTypes
+import src.main.kotlin.Nodes.ExprNode
 import kotlin.reflect.KClassifier
 
-class PairLitNode(val ctx : BasicParser.PairLitContext): Node {
+class PairLitNode(val ctx : BasicParser.PairLitContext): ExprNode {
 
     override fun syntaxCheck() {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
@@ -15,7 +17,7 @@ class PairLitNode(val ctx : BasicParser.PairLitContext): Node {
     override fun semanticCheck(errors: ErrorLogger, table: SymbolTable) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
-fun getType(): BaseNode {
+    override fun getType(): LitTypes {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
