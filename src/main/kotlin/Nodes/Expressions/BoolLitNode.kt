77d20c73f8156056505573a7aa0f440f051d04ad
nahida <nc1517@ic.ac.uk>
2019-02-11 21:54:26 +0000

    added visit for while and modified ident semantic checks

--- src/main/kotlin/Nodes/Expressions/BoolLitNode.kt
+++ src/main/kotlin/Nodes/Expressions/BoolLitNode.kt
@@ -5,18 +5,19 @@ import main.kotlin.Nodes.BaseNode
 import main.kotlin.SymbolTable
 import main.kotlin.Utils.LitTypes
 import src.main.kotlin.Nodes.ExprNode
-
+import main.kotlin.Errors.UnknownIdentifier
 
 
 class BoolLitNode(val bool_val : String, override val ctx: BasicParser.BoolLitContext) : ExprNode {
-
-
 
     override fun getType() : LitTypes {
         return LitTypes.BoolWacc
     }
 
     override fun semanticCheck(errors: ErrorLogger, table: SymbolTable) {
+        if (!bool_val.equals("true") && !bool_val.equals("false")) {
+            errors.addError(UnknownIdentifier(ctx.start.line, ctx.start.charPositionInLine))
+        }
     }
 
     override fun syntaxCheck() {
