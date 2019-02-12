009b73895153eb6ca6c3c4e741a962c036b7033b
Hubert Kaluzny <contact@hubertkaluzny.com>
2019-02-12 22:57:41 +0000

    Remove unecessary code from println

--- src/main/kotlin/Nodes/Statement/PrintLnStatNode.kt
+++ src/main/kotlin/Nodes/Statement/PrintLnStatNode.kt
@@ -19,42 +19,6 @@ class PrintLnStatNode(val expr : ExprNode, override val ctx: BasicParser.Println
     }
 
     override fun semanticCheck(errors: ErrorLogger, table: SymbolTable) {
-        if(expr !is UnaryOpNode) {
-
-        if (expr.getType() == LitTypes.IdentWacc) {
-            val value = table.lookupSymbol((expr as IdentNode).id)
-            if (value == null) {
-                errors.addError(UndefinedVariable(ctx, expr.id))
-                return
-            } else {
-                var v = value
-                while(v != null && v?.getType() == LitTypes.IdentWacc) {
-                    if(v is PairElemNode) {
-                        v = v.expr
-                    }
-                    v = table.lookupSymbol((v as IdentNode).id)
-                }
-                if ( v == null ) {
-                    errors.addError(UndefinedVariable(ctx, (expr).id))
-                    return
-                }
-                if (v.getType() != LitTypes.StringWacc
-                        && v.getType() != LitTypes.CharWacc
-                        && v.getType() != LitTypes.IntWacc
-                        && v.getType() != LitTypes.BoolWacc
-                        && v.getType() != LitTypes.PairWacc) {
-                    errors.addError(IncompatibleTypes(ctx, "{STRING, INT, CHAR, PAIR, BOOL}", v, table))
-                    return
-                }
-            }
-        } else if (expr.getType() != LitTypes.StringWacc
-                && expr.getType() != LitTypes.CharWacc
-                && expr.getType() != LitTypes.IdentWacc
-                && expr.getType() != LitTypes.IntWacc
-                && expr.getType() != LitTypes.BoolWacc
-                && expr.getType() != LitTypes.PairWacc) {
-            errors.addError(IncompatibleTypes(ctx, "{STRING, INT, CHAR, PAIR, BOOL}", expr, table))
-        }}
         expr.semanticCheck(errors, table)
     }
 }
