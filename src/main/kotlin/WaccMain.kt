f16290ab716b191d9b57b7e52048636ce3451f56
Blanca <bt3917@ic.ac.uk>
2019-02-13 16:00:25 +0000

    the advanced tictactoe needs the return statement checked with ifs

--- src/main/kotlin/WaccMain.kt
+++ src/main/kotlin/WaccMain.kt
@@ -10,7 +10,7 @@ fun main(args: Array<String>) {
         if (args.size == 0) {
                 System.setIn(FileInputStream("../wacc_examples/" +
 
-                        "valid/while/whileBoolFlip.wacc"))
+                        "/valid/advanced/ticTacToe.wacc"))
 
         } else {
                 System.setIn(FileInputStream(args[0]))
