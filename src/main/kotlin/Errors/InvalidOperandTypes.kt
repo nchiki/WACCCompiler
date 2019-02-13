cac46592fbaca7d381c604c333c8b71d91fe56e7
Blanca <bt3917@ic.ac.uk>
2019-02-13 13:58:44 +0000

    fixed merge conflicts

--- src/main/kotlin/Errors/InvalidOperandTypes.kt
+++ src/main/kotlin/Errors/InvalidOperandTypes.kt
@@ -5,7 +5,7 @@ import org.antlr.v4.runtime.ParserRuleContext
 class InvalidOperandTypes(ctx : ParserRuleContext) : SemanticError(ctx) {
 
     override fun printError(): String {
-        return "Invalid operand types"
+        return "Invalid operand types at ${ctx.start.line} ${ctx.start.charPositionInLine}"
     }
 
 }
