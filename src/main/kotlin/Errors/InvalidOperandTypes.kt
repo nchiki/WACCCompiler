ac8838044f363405c0680f2d0b03483734ce4eae
Lloyd <lloyd.cllwes@gmail.com>
2019-02-13 15:55:10 +0000

    Change InvalidOperatorTypes to take in expected and actual types:

--- src/main/kotlin/Errors/InvalidOperandTypes.kt
+++ src/main/kotlin/Errors/InvalidOperandTypes.kt
@@ -2,10 +2,10 @@ package Errors
 
 import org.antlr.v4.runtime.ParserRuleContext
 
-class InvalidOperandTypes(ctx : ParserRuleContext) : SemanticError(ctx) {
+class InvalidOperandTypes(ctx : ParserRuleContext, val expected : String, val actual : String) : SemanticError(ctx) {
 
     override fun printError(): String {
-        return "Invalid operand types at ${ctx.start.line} ${ctx.start.charPositionInLine}"
+        return super.printError() + "Invalid operand types (expected: $expected, actual: $actual"
     }
 
 }
