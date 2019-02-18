f000ebc2797b07b6066476d066203505d92c7f2a
Blanca <bt3917@ic.ac.uk>
2019-02-18 14:40:37 +0000

    added branch and ldr instr

--- src/main/kotlin/Instructions/LoadInstr.kt
+++ src/main/kotlin/Instructions/LoadInstr.kt
@@ -4,9 +4,8 @@ import main.kotlin.Utils.Register
 
 class LoadInstr(val arg1 : Register, val arg2: Any) : Instruction {
 
-
     override fun getString(): String {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return "LDR ${arg1.toString()} ${arg2.toString()}"
     }
 
 }
--- src/main/kotlin/Instructions/MovInstr.kt
+++ src/main/kotlin/Instructions/MovInstr.kt
@@ -2,7 +2,7 @@ package main.kotlin.Instructions
 
 import main.kotlin.Utils.Condition
 
-class MovInstr(val value1 : Any, val dest : Any, val cond : Condition) : Instruction{
+class MovInstr(val value1 : Any, val dest : Any, val cond : Condition = Condition.NULL) : Instruction{
 
     override fun getString() : String {
         return "MOV${cond.toString()} ${value1.toString()} ${dest.toString()}"
