3be2381a8be44acf5fb5127f8b599ecf645e5893
nahida <nc1517@ic.ac.uk>
2019-02-18 08:05:14 +0000

    added basic instruction classes and interface as well as starting generateCode for Print

--- src/main/kotlin/Nodes/Statement/PrintStatNode.kt
+++ src/main/kotlin/Nodes/Statement/PrintStatNode.kt
@@ -12,13 +12,23 @@ import main.kotlin.SymbolTable
 import main.kotlin.Utils.LitTypes
 import src.main.kotlin.Nodes.ExprNode
 import src.main.kotlin.Nodes.Literals.IntLitNode
+import kotlin.Instructions.Instruction
+import kotlin.Instructions.MovInstr
 
 class PrintStatNode(val expr : ExprNode, override val ctx : BasicParser.PrintContext) : Node{
     override val weight: Int
         get() = TODO("not implemented") //To change initializer of created properties use File | Settings | File Templates.
 
     override fun generateCode(codeGeneration: CodeGeneration) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        var instrs = arrayListOf<Instruction>()
+        instrs.add(MovInstr())
+        if (expr.getType() == LitTypes.CharWacc || expr.getType() == LitTypes.StringWacc) {
+            //add case for Bool
+        } else if (expr.getType() == LitTypes.BoolWacc) {
+            //add all these cases
+        } else if (expr.getType() == LitTypes.IntWacc) {
+            //add all these case
+        }
     }
 
     override fun translate() {
