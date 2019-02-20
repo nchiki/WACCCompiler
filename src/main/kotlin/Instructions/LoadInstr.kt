08c47ad0dfbebacc183d8b13dfbb2ae26e29679e
nahida <nc1517@ic.ac.uk>
2019-02-20 13:34:13 +0000

    added check in load instr for register

--- src/main/kotlin/Instructions/LoadInstr.kt
+++ src/main/kotlin/Instructions/LoadInstr.kt
@@ -7,13 +7,18 @@ class LoadInstr(val arg1 : Register, val arg2: Any) : Instruction {
 
     override fun getString(): String {
         if (arg2 is Int || arg2 is String) {
-            return "LDR ${arg1.toString()}, =${arg2}"
+            return "LDR $arg1, =$arg2"
         }
         if (arg2 is IntLitNode) {
-            return "LDR ${arg1.toString()}, =${arg2.int_val.toString()}"
+            return "LDR $arg1, =${arg2.int_val}"
         }
+        if (arg2 is Register) {
+            return "LDR $arg1, [$arg2]"
+        }
+        //default case
         else {
-            return "LDR ${arg1.toString()}, ${arg2.toString()}"
+            return "LDR $arg1, $arg2"
+
         }
     }
 }
