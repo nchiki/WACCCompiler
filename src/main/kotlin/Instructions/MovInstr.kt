f1f7313e94087de935a1f54aedfce31b91290604
Blanca <bt3917@ic.ac.uk>
2019-02-20 14:35:11 +0000

    added comments

--- src/main/kotlin/Instructions/MovInstr.kt
+++ src/main/kotlin/Instructions/MovInstr.kt
@@ -3,14 +3,11 @@ package main.kotlin.Instructions
 import main.kotlin.Utils.Condition
 import main.kotlin.Utils.Register
 
-class MovInstr(val dest : Any, val val1 : Any, val cond : Condition?) : Instruction{
+class MovInstr(val dest : Any, val val1 : Any, val cond : Condition? = Condition.NULL) : Instruction{
 
     override fun getString() : String {
-        if (cond != null) {
-            return "MOV${cond.toString()} ${dest.toString()}, ${val1.toString()}"
-        }
-        return "MOV ${dest.toString()}, ${val1.toString()}"
 
+        return "MOV${cond.toString()} $dest, $val1"
 
     }
 
