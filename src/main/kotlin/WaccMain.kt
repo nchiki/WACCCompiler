fe5b81765989a8c80edae4cb1d19ee9de12c90dc
Blanca <bt3917@ic.ac.uk>
2019-02-26 10:39:27 +0000

    changes

--- src/main/kotlin/WaccMain.kt
+++ src/main/kotlin/WaccMain.kt
@@ -10,7 +10,7 @@ import kotlin.system.exitProcess
 fun main(args: Array<String>) {
         if (args.size == 0) {
                 System.setIn(FileInputStream("../wacc_examples/" +
-                        "/valid/expressions/orExpr.wacc"))
+                        "/valid/expressions/longExpr2.wacc"))
 
 
 
@@ -45,8 +45,8 @@ fun main(args: Array<String>) {
         codeGen.initRegs()
         progNode.generateCode(codeGen)
 
-        //codeGen.writeToFile("print.s")
-        codeGen.writeToFile(args[0].substring(args[0].lastIndexOf("/") + 1).replace(".wacc", ".s"))
+        codeGen.writeToFile("print.s")
+        //codeGen.writeToFile(args[0].substring(args[0].lastIndexOf("/") + 1).replace(".wacc", ".s"))
 
 }
 
