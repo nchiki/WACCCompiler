b4802b5009e466b5d4d34c805c3d33db3d2cdf6e
Lloyd Clowes <lc5017@ic.ac.uk>
2019-02-27 09:48:23 +0000

    Continue writing bash script

--- test_backend.sh
+++ test_backend.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 
 EXIT="tests/valid/basic/exit"
+PRINT="tests/valid/IO/print"
 
 DIRECTORY="tests/"
 
 if [[ $1 == "exit" ]]
 then
     DIRECTORY=$EXIT
+elif [[ $1 == "print" ]]
+then
+    DIRECTORY=$PRINT
 fi
 
 TESTS=$(find $DIRECTORY -name "*.wacc" | wc -l)
@@ -14,10 +18,12 @@ TESTS=$(find $DIRECTORY -name "*.wacc" | wc -l)
 find $DIRECTORY -name "*.wacc" | (
     FAILED=0
     while read fname; do
-        sh compile $fname > /dev/null 2>&1
+        sh compile $fname
         SHORTENED=${fname::$((${#fname} - 5))}
         SHORTENED=${SHORTENED##*/}
-        sh arm-linux-gnueabi-gcc -o $SHORTENED -mcpu=arm1176jzf-s -mtune=arm1176jzf-s "$SHORTENED.s"
+        echo $SHORTENED
+	      eval $(arm-linux-gnueabi-gcc -o $SHORTENED -mcpu=arm1176jzf-s -mtune=arm1176jzf-s "$SHORTENED.s")
+        echo end
         if [[ !($(sh qemu-arm -L /usr/arm-linux-gnueabi/ $SHORTENED) -eq $(sh ruby refCompile fname -x)) ]]
         then
             echo "$fname test failed, expected  but returned "
