733bf98dfa9dd0a7e10948f80995c962f7d66bdd
Nahida  Chiki <nahida.chiki17@imperial.ac.uk>
2019-02-28 14:27:34 +0000

    Revert "Merge branch 'lloyd/tempFix' into 'master'"
    This reverts merge request !364

--- src/main/kotlin/WaccVisitor.kt
+++ src/main/kotlin/WaccVisitor.kt
@@ -263,12 +263,7 @@ class WaccVisitor : BasicParserBaseVisitor<Node>() {
     override fun visitUnOp(ctx: BasicParser.UnOpContext): Node {
         val ex = ctx.expr()
         if (ex is BasicParser.IntLitContext) {
-            println(ctx.text)
-            println(ctx.expr().text)
-            if (ctx.text[0] == '=') {
-                return IntLitNode(-ex.INT_LIT().text.toLong(), ex)
-            }
-            return IntLitNode(ex.INT_LIT().text.toLong(), ex)
+            return IntLitNode(-ex.INT_LIT().text.toLong(), ex)
         }
         val operand = visit(ctx.expr()) as ExprNode
         val operator = ctx.unaryOper()
