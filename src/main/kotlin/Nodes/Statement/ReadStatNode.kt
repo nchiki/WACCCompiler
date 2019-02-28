fe5a77ba8b82a9e926a1ffda7044c97b530ca7c9
lc5017 <lc5017@ic.ac.uk>
2019-02-28 17:38:48 +0000

    Potentially fix things

--- src/main/kotlin/Nodes/Statement/ReadStatNode.kt
+++ src/main/kotlin/Nodes/Statement/ReadStatNode.kt
@@ -29,11 +29,7 @@ class ReadStatNode(private val lhs: LHS_Node, override val ctx: BasicParser.Read
         if (lhs.Nodetype is IdentNode) {
             lhs.generateCode(codeGenerator)
         }
-        codeGenerator.addInstruction(codeGenerator.curLabel, LoadInstr(codeGenerator.regsNotInUse.peek(), "[sp]", null))
-        codeGenerator.addInstruction(codeGenerator.curLabel, MovInstr(Register.r0, codeGenerator.regsNotInUse.peek()))
-        codeGenerator.addInstruction(codeGenerator.curLabel, BLInstr("p_check_null_pointer"))
-        codeGenerator.addHelper("p_check_null_pointer")
-        codeGenerator.addError(NullReferDef)
+
         val label: String
         var type = symbolTable!!.lookupSymbol(lhs.id)
         if (lhs.Nodetype is PairElemNode) {
@@ -54,6 +50,11 @@ class ReadStatNode(private val lhs: LHS_Node, override val ctx: BasicParser.Read
         if (type != null) {
             addInstructions(codeGenerator, type.getBaseType(), label)
         }
+        codeGenerator.addInstruction(codeGenerator.curLabel, LoadInstr(codeGenerator.regsNotInUse.peek(), "[sp]", null))
+        codeGenerator.addInstruction(codeGenerator.curLabel, MovInstr(Register.r0, codeGenerator.regsNotInUse.peek()))
+        codeGenerator.addInstruction(codeGenerator.curLabel, BLInstr("p_check_null_pointer"))
+        codeGenerator.addHelper("p_check_null_pointer")
+        codeGenerator.addError(NullReferDef)
     }
 
     fun addInstructions(codeGenerator: CodeGenerator, type : LitTypes, printLabel : String) {
