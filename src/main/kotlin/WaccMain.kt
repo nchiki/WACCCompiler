6e775c16b572dcb59ac68e877078e755e333c0c5
Hubert Kaluzny <hk4817@ic.ac.uk>
2019-03-15 12:22:22 +0000

    Add optimisation flag to wacc main

--- src/main/kotlin/WaccMain.kt
+++ src/main/kotlin/WaccMain.kt
@@ -1,6 +1,7 @@
 import main.kotlin.CodeGenerator
 import main.kotlin.ErrorLogger
 import main.kotlin.SymbolTable
+import main.kotlin.ValueTable
 import org.antlr.v4.runtime.CharStreams
 import org.antlr.v4.runtime.CommonTokenStream
 import java.io.FileInputStream
@@ -8,42 +9,54 @@ import kotlin.system.exitProcess
 
 
 fun main(args: Array<String>) {
-        if (args.size == 0) {
-                System.setIn(FileInputStream("../wacc_examples/" +
-          "valid/function/high_order/ownHighOrd.wacc"))
-        } else {
-                System.setIn(FileInputStream(args[0]))
-        }
-        val input = CharStreams.fromStream(java.lang.System.`in`)
-        //Lexical analysis
-        val lexer = BasicLexer(input)
-        //Create a buffer of tokens
-        val tokens = CommonTokenStream(lexer)
-        //Syntactical analysis
-        val parser = BasicParser(tokens)
-        val tree = parser.prog()
-        //Exit with code 100 if there are any syntax errors
-        if (parser.numberOfSyntaxErrors > 0) {
-                exitProcess(100)
-        }
-        val visitor = WaccVisitor()
-        val errorLogger = ErrorLogger()
-        val symbolTable = SymbolTable(null)
-        val progNode = visitor.visit(tree)
-        //Semantic Check
-        progNode.semanticCheck(errorLogger, symbolTable)
-        for (error in errorLogger.errorList) {
-                println(error.printError())
-        }
-        if (errorLogger.errorList.count() > 0) {
-                exitProcess(200)
-        }
-        val codeGen = CodeGenerator()
-        codeGen.initRegs()
-        codeGen.switchFunctions("main")
-        progNode.generateCode(codeGen)
 
-        codeGen.writeToFile(args[0].substring(args[0].lastIndexOf("/") + 1).replace(".wacc", ".s"))
+    var optimise = false
+
+    if (args.size > 1) {
+        if (args[0].equals("-o")) {
+            optimise = true
+        }
+        System.setIn(FileInputStream(args[1]))
+    }else{
+        System.setIn(FileInputStream(args[0]))
+    }
+
+    val input = CharStreams.fromStream(java.lang.System.`in`)
+    //Lexical analysis
+    val lexer = BasicLexer(input)
+    //Create a buffer of tokens
+    val tokens = CommonTokenStream(lexer)
+    //Syntactical analysis
+    val parser = BasicParser(tokens)
+    val tree = parser.prog()
+    //Exit with code 100 if there are any syntax errors
+    if (parser.numberOfSyntaxErrors > 0) {
+        exitProcess(100)
+    }
+    val visitor = WaccVisitor()
+    val errorLogger = ErrorLogger()
+    val symbolTable = SymbolTable(null)
+    val progNode = visitor.visit(tree)
+    //Semantic Check
+    progNode.semanticCheck(errorLogger, symbolTable)
+    for (error in errorLogger.errorList) {
+        println(error.printError())
+    }
+    if (errorLogger.errorList.count() > 0) {
+        exitProcess(200)
+    }
+
+    if(optimise){
+        val valueTable = ValueTable(null)
+        progNode.optimise(valueTable)
+    }
+
+    val codeGen = CodeGenerator()
+    codeGen.initRegs()
+    codeGen.switchFunctions("main")
+    progNode.generateCode(codeGen)
+
+    codeGen.writeToFile(args[0].substring(args[0].lastIndexOf("/") + 1).replace(".wacc", ".s"))
 
 }
 
